79c925b9431344633ffc93f3843ef600311d3494
nesaratai <nesaratai@gmail.com>
2025-01-19 20:25:41 -0500

    new lecture

---
+++ app.js
@@ -0,0 +1,24 @@
+// this is singel comment.
+
+
+console.log("hello world!");
+
+let log;
+log = "new";
+
+let favsnack;
+favsnack = "lays";
+
+const name = "Nesar";
+
+console.log("My name is " + name + " and my fav snack is: " + favsnack + " " + log);
+
+let greetings = "Hi Brother."
+let theAnswer = "Good, How are you."
+let theSecondQ = "name pets \"your pet\" like you want"
+
+
+console.log(theSecondQ)
+console.log(greetings + theAnswer + theSecondQ)
+
+
